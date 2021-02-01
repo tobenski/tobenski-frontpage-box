2d07d6722b112d36fa1efe8d5d202c6c8ff5049c
Knud Rishøj <knud@det-gamle-posthus.dk>
2021-02-01 10:01:35 +0100

    version bumb

--- tobenski-frontpage-box.php
+++ tobenski-frontpage-box.php
@@ -16,7 +16,7 @@
  * Plugin Name:       Det Gamle Posthus - Frontpage Box
  * Plugin URI:        https://github.com/tobenski/tobenski-frontpage-box
  * Description:       This plugin adds boxes to the frontpage.
- * Version:           1.0.2
+ * Version:           1.1.0
  * Author:            Knud Rihøj
  * Author URI:        https://github.com/tobenski/
  * License:           GPL-2.0+
@@ -35,7 +35,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'TOBENSKI_FRONTPAGE_BOX_VERSION', '1.0.2' );
+define( 'TOBENSKI_FRONTPAGE_BOX_VERSION', '1.1.0' );
 
 /**
  * The code that runs during plugin activation.
